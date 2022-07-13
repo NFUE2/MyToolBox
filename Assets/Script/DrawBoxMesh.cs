4705aa988668bbd0af0a6d0e2bbb8652c1c6af99
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2022-07-13 17:47:11 +0900

    박스 매쉬 그리기
    박스 매쉬 그리기

---
+++ Assets/Script/DrawBoxMesh.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBoxMesh : MonoBehaviour
+{
+    Mesh mesh;
+    public Vector3[] Pos;
+
+    MeshFilter MF;
+
+    private void Start()
+    {
+        MF = GetComponent<MeshFilter>();
+        mesh = new Mesh();
+
+        Pos = new Vector3[4];
+        Pos[0] = Vector3.zero;
+        Pos[1] = Vector3.forward;
+        Pos[2] = Vector3.forward + Vector3.right;
+        Pos[3] = Vector3.right;
+
+        //Vector3 배열형
+        mesh.vertices = Pos;
+
+        int[] vertices = { 0,1,2,0,2,3 };
+        //int 배열형
+
+        mesh.triangles = vertices;
+        MF.mesh = mesh;
+    }
+}
---
+++ Assets/Script/DrawLine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    [Range(0.0f,360.0f)]
+    public float angle;
+
+    private void OnDrawGizmos()
+    {
+        Line();
+    }
+
+    void Line()
+    {
+        Gizmos.color = Color.red;
+        float angle2 = angle * Mathf.Deg2Rad;
+        Vector3 L = new Vector3(Mathf.Sin(angle2),0.0f,Mathf.Cos(angle2)) * 5.0f;
+        Debug.DrawLine(transform.position,L);
+    }
+}
