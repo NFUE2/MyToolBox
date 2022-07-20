0d74cee54198996db66c631a942eb17057053d05
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2022-07-20 22:22:47 +0900

    선그리기 및 메테리얼
    선그리기 및 메테리얼

--- Assets/Script/DrawLine.cs
+++ Assets/Script/DrawLine.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class DrawLine : MonoBehaviour
 {
-    [Range(0.0f,360.0f)]
-    public float angle;
+    //[Range(0.0f, 360.0f)]
+    //public float angle;
 
     private void OnDrawGizmos()
     {
-        Line();
+        //Line();
+
+        float angle = -45.0f * Mathf.Deg2Rad;
+
+        Vector3 Pos = transform.position;
+
+        for (int i = 0; i < 2; i++)
+        {
+            float angle2 = (90.0f * i) * Mathf.Deg2Rad;
+            Vector3 L = new Vector3(Mathf.Sin(angle + angle2),0.0f,Mathf.Cos(angle + angle2)) * 5.0f;
+
+            Gizmos.DrawLine(Pos,L);
+            Pos = L;
+        }
+        Gizmos.DrawLine(Pos,transform.position);
     }
 
     void Line()
     {
-        Gizmos.color = Color.red;
-        float angle2 = angle * Mathf.Deg2Rad;
-        Vector3 L = new Vector3(Mathf.Sin(angle2),0.0f,Mathf.Cos(angle2)) * 5.0f;
-        Debug.DrawLine(transform.position,L);
+        //Gizmos.color = Color.red;
+        //float angle2 = angle * Mathf.Deg2Rad;
+        //Vector3 L = new Vector3(Mathf.Sin(angle2), 0.0f, Mathf.Cos(angle2)) * 5.0f;
+        //Gizmos.DrawLine(transform.position, L);
     }
 }
